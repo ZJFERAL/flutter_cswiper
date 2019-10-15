3124dbddc162fded78a0d0ca6ee9e1a9b43039d5
jifeng <jifeng@ftsafe.com>
2019-10-15 20:31:25 +0800

    Initial commit

---
+++ lib/flutter_cswiper.dart
@@ -0,0 +1,7 @@
+library flutter_cswiper;
+
+export 'src/swiper.dart';
+export 'src/swiper_pagination.dart';
+export 'src/swiper_control.dart';
+export 'src/swiper_controller.dart';
+export 'src/swiper_plugin.dart';
